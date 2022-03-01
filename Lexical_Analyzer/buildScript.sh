f56d5dc710426d20a1ba7d5535ed03cbf94cabf5
Tehmike <mbastakis@gmail.com>
2022-03-01 16:22:48 +0200

    Finished the incomplete string function, added a new return value in alpha_yylex()

--- Lexical_Analyzer/buildScript.sh
+++ Lexical_Analyzer/buildScript.sh
@@ -3,4 +3,8 @@
 cd lexical_scanner
 flex --outfile=alpha_lexical_scanner.c lexical_scanner.l
 cd ..
-g++ -o al lexical_scanner/alpha_lexical_scanner.c
+if g++ -o al lexical_scanner/alpha_lexical_scanner.c; then
+echo "Compiled successfully.";
+else
+echo "Compilation failed";
+fi
