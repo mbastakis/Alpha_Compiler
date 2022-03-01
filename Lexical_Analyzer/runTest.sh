e4243b905999d764d0808acee436c16503d92ca3
Tehmike <mbastakis@gmail.com>
2022-03-01 19:02:31 +0200

    added tests for operators

--- Lexical_Analyzer/runTest.sh
+++ Lexical_Analyzer/runTest.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
 test=$1
-
+clear
+echo ---------------------- INPUT -----------------------
+cat -n tests/$1.test
+echo -e '\n'
+echo ---------------------- OUTPUT ----------------------
 ./dist/al tests/$1.test
