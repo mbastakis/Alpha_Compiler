8661f651152c80ab783dc6012b446d5a9e44f65a
Tehmike <mbastakis@gmail.com>
2022-04-01 18:50:25 +0300

    New methods for Symbols

--- Syntactic_Analyzer/public/Symbol.hpp
+++ Syntactic_Analyzer/public/Symbol.hpp
@@ -5,12 +5,15 @@
 #include <iostream>
 
 typedef enum {
-    GLOBAL_VARIABLE,
+    GLOBAL_VARIABLE = 0,
     LOCAL_VARIABLE,
     FORMAL_ARGUMENT,
     LIBRARY_FUNCTION,
     USER_FUNCTION
 } Symbol_T;
+
+static const char* enum_str[] =
+{ "global variable", "local variable", "formal argument", "library function", "user function" };
 
 class Symbol {
 
@@ -24,7 +27,7 @@ public:
     Symbol() = default;
     ~Symbol() = default;
 
-    Symbol(std::string id, Symbol_T type, unsigned int scope, unsigned int line) {
+    Symbol(std::string id, Symbol_T type, unsigned int line, unsigned int scope) {
         m_id = id;
         m_type = type;
         m_scope = scope;
@@ -45,6 +48,21 @@ public:
 
     unsigned int getScope() {
         return m_scope;
+    }
+
+    std::string toString() {
+        std::string qm = "\"";
+        std::string lb = "[";
+        std::string rb = "]";
+        std::string lp = "(";
+        std::string rp = ")";
+        std::string sp = " ";
+
+        return (qm + m_id + qm + sp + \
+            lb + enum_str[m_type] + rb + sp + \
+            lp + "line " + std::to_string(m_line) + rp + sp + \
+            lp + "scope " + std::to_string(m_scope) + rp + sp
+            );
     }
 
 
