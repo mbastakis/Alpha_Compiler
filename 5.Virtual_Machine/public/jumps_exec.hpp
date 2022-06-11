2f6ac65e6ebd04e67b47daa7cf170f08c4d3037d
marileni <marilenisin1@gmail.com>
2022-06-11 16:29:03 +0300

    bools

--- 5.Virtual_Machine/public/jumps_exec.hpp
+++ 5.Virtual_Machine/public/jumps_exec.hpp
@@ -4,6 +4,28 @@
 #include "Instruction.hpp"
 #include "AVM.hpp"
 
+typedef unsigned char (*tobool_func_t)(avm_memcell*); //unsign int?
+
+unsigned char number_tobool (avm_memcell* m) { return m->data != 0; }
+unsigned char string_tobool (avm_memcell* m) { return m->data[0] != 0; }
+unsigned char bool_tobool (avm_memcell* m) { return m->data; }
+unsigned char table_tobool (avm_memcell* m) { return 1; }
+unsigned char userfunc_tobool (avm_memcell* m) { return 1; }
+unsigned char libfunc_tobool (avm_memcell* m) { return 1; }
+unsigned char nill_tobool (avm_memcell* m) { return 0; }
+unsigned char undef_tobool (avm_memcell* m) { assert(0); return 0; }
+
+tobool_func_t toboolFuncs[] = {
+    number_tobool, string_tobool, bool_tobool, 
+    table_tobool, userfunc_tobool, libfunc_tobool,
+    nill_tobool, undef_tobool
+}
+
+unsigned char avm_tobool (avm_memcell* m) {
+    assert(m->type >= 0 && m->type < UNDEFINED_M);
+    return (*tobool_funcs[m->type]) (m);
+}
+
 extern void execute_jeq(Instruction*);
 extern void execute_jne(Instruction*);
 extern void execute_jle(Instruction*);
